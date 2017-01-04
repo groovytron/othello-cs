a2d8bb9f6f7ad1cbd4454523e4d8dd6be8c40f2e
arnaud droxler <arnaud.droxler@gmail.com>
2017-01-04 11:20:45 +0100

    creation des 2 classes player et game

---
+++ Othello/Othello/Game.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloConsole
+{
+    class Game : IPlayable
+    {
+
+        private Player[] players;
+        private int[,] board;
+
+
+        public Game()
+        {
+
+        }
+
+        public void save()
+        {
+
+        }
+
+        public void load()
+        {
+
+        }
+
+        public void newGame()
+        {
+
+        }
+
+        public void pause()
+        {
+
+        }
+
+        public int getBlackScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<char, int> getNextMove(int[,] game, int level, bool whiteTurn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getWhiteScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isPlayable(int column, int line, bool isWhite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool playMove(int column, int line, bool isWhite)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Othello/Othello/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloConsole
+{
+    class Player
+    {
+        private int time;
+        private int score;
+
+        public Player()
+        {
+            
+        }
+
+        public int Time
+        {
+            get
+            {
+                return time;
+            }
+
+            set
+            {
+                time = value;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+
+            set
+            {
+                score = value;
+            }
+        }
+
+    }
+}
